acb4c05874779659ce6825ec2d13500704f1910d
Rico Putra Pradana <ricopradanaputra2198@gmail.com>
2018-09-18 14:21:46 +0700

    Done Latihan Project Viral

--- src/main/java/com/example/demo/controller/PageController.java
+++ src/main/java/com/example/demo/controller/PageController.java
@@ -3,10 +3,11 @@ package com.example.demo.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+
 @Controller
 public class PageController {
-	@RequestMapping("/viral")
+	@RequestMapping("/viral") // menerima pemanggilan alamat dengan akhiran "/viral"
 	public String index() {
-		return "viral";
+		return "viral"; // memanggil file html dengan nama sesuai return tsb
 	}
 }
