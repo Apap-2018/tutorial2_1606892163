ba212ce24186dbf8e79fee4f80bc06bef754e771
Rico Putra Pradana <ricopradanaputra2198@gmail.com>
2018-09-18 15:11:31 +0700

    Done Latihan Request Parameter

--- src/main/java/com/example/demo/controller/PageController.java
+++ src/main/java/com/example/demo/controller/PageController.java
@@ -1,7 +1,9 @@
 package com.example.demo.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 
 @Controller
@@ -9,5 +11,11 @@ public class PageController {
 	@RequestMapping("/viral") // menerima pemanggilan alamat dengan akhiran "/viral"
 	public String index() {
 		return "viral"; // memanggil file html dengan nama sesuai return tsb
+	}
+	
+	@RequestMapping("/challenge")
+	public String challenge(@RequestParam(value = "name") String name, Model model) {
+		model.addAttribute("name", name);
+		return "challenge";
 	}
 }
