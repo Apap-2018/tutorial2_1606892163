c94463e57b8fb236afd0547623ca18d05eeadb4c
Rico Putra Pradana <ricopradanaputra2198@gmail.com>
2018-09-18 23:17:27 +0700

    Try Path Variable

--- src/main/java/com/example/demo/controller/PageController.java
+++ src/main/java/com/example/demo/controller/PageController.java
@@ -2,6 +2,7 @@ package com.example.demo.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -15,6 +16,12 @@ public class PageController {
 	
 	@RequestMapping("/challenge")
 	public String challenge(@RequestParam(value = "name") String name, Model model) {
+		model.addAttribute("name", name);
+		return "challenge";
+	}
+	
+	@RequestMapping("/challenge/{name}")
+	public String challengePage(@PathVariable String name, Model model) {
 		model.addAttribute("name", name);
 		return "challenge";
 	}
